42cce08fdbd795bf37a6259495a7b4b471852bf5
Ryan7931 <9024915@student.zadkine.nl>
2025-11-04 14:57:46 +0100

    homepagina

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,20 @@
 import './home.css'
-import profileImage from '../assets/profiel.jpg' // Replace with your actual image name
+import profileImage from '../assets/profiel.jpg' 
 
 const Home = () => {
   return (
-    <div>
-      <h1 id="NameH1">Ryan Kalisvaart</h1>
-      <h2 id="SDH2">software developer</h2>
-      <h3 id="SDH3">Beginnende software developer <br /> met veel talen om te leren</h3>
-      <img src={profileImage} alt="Profile" className="profile-image" />
-    </div>
-
+    <main className="home">
+      <section className="home-text">
+        <div className="NameSD">
+          <h1 id="NameH1">Ryan Kalisvaart</h1>
+          <h2 id="SDH2">software developer</h2>
+        </div>
+        <h3 id="SDH3">Beginnende software developer <br /> met veel talen om te leren</h3>
+      </section>
+      <aside className="home-media">
+        <img src={profileImage} alt="Profile" className="profile-image" />
+      </aside>
+    </main>
   );
 };
 
